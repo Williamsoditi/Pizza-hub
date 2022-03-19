e8384f5870c42122730b280d451d6726fc878168
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-19 11:03:22 +0300

    Feat : Adds services - This commit adds the services section

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".delivery").click(function () {
+        $(".delivery-img").fadeToggle(1000);
+        $("#delivery").fadeToggle(1000);
+    });
+    $(".payment").click(function () {
+        $(".payment-img").fadeToggle(1000);
+        $("#payment").fadeToggle(1000);
+    });
+    $(".offers").click(function () {
+        $(".offers-img").fadeToggle(1000);
+        $("#offers").fadeToggle(1000);
+    });
+    $(".hotels").click(function () {
+        $(".trial").fadeToggle(1000);
+        $("#hotels").fadeToggle(1000);
+    });
+})
